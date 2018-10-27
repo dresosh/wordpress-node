363fcaad62e101505f1861b61b616aa29550d6af
Andre Soschinski <dresosh@users.noreply.github.com>
2018-10-26 22:44:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,8 @@ const express = require( 'express' )
 const epf = require( 'express-php-fpm' ).default
 
 const options = {
-	// root of your php files
 	documentRoot: __dirname + '/wordpress',
-
-	// extra env variables
 	env: {},
-
-	// connection to your php-fpm server
-	// https://nodejs.org/api/net.html#net_socket_connect_options_connectlistener
 	socketOptions: { port: 9000 },
 }
 
